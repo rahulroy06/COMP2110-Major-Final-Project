d6404b2bfa598eee8c38441e73731b4fe54b7c0a
Aaron Wilson <shyguyman11@gmail.com>
2024-04-10 12:00:52 +1000

    Weekly Summary - Add initial widget code

---
+++ src/components/weekly-summary.js
@@ -0,0 +1,24 @@
+import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+
+class WeeklySummary extends LitElement {
+    static styles = css`
+        host: {
+            display: block;
+            width: 250px;
+            height: 250px;
+            background-color: 
+        }
+    `;
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return html`
+            <h3>Weekly Summary</h3>
+        `;
+    }
+}
+
+customElements.define('weekly-summary', WeeklySummary);

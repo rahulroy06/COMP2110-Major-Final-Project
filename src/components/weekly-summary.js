bb288f03e325f6c58bf32c3cc062b7c53dbfd986
Aaron Wilson <shyguyman11@gmail.com>
2024-04-16 15:50:03 +1000

    Weekly Summary - Add initial functionality for displaying weekly tasks

--- src/components/weekly-summary.js
+++ src/components/weekly-summary.js
@@ -1,6 +1,10 @@
 import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
-
+import {TaskModel} from '../models.js';
 class WeeklySummary extends LitElement {
+    static properties = {
+        _weeklyTasks: {state: true}
+    };
+
     static styles = css`
         :host {
             display: block;
@@ -21,9 +25,25 @@ class WeeklySummary extends LitElement {
         super();
     }
 
+    _displayTasks() {
+        const weekStart = new Date();
+        weekStart.setDate(weekStart.getDate() - weekStart.getDay() + 1);
+
+        window.addEventListener('tasks', () => {
+            this._weeklyTasks = TaskModel.getTasksForDay(weekStart);
+            console.log("Got Tasks.");
+        })
+        if (this._weeklyTasks) {
+            return html `<p>Weekly Tasks: ${this._weeklyTasks.length}</p>`
+        } else {
+            return html `<p>Loading Weekly Summary...</p>`
+        }
+    }
+
     render() {
         return html`
             <h3>Weekly Summary</h3>
+            ${this._displayTasks()}
         `;
     }
 }

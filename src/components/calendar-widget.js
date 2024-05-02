e194ff206133ef9798d62966bd00946f90bc8fb8
2Ayy <joshua.philips@students.mq.edu.au>
2024-05-02 19:34:14 +1000

    added DaysInMonth (output array of days per month) and DueToday (check if any tasks are due on specified day).

--- src/components/calendar-widget.js
+++ src/components/calendar-widget.js
@@ -1,10 +1,13 @@
-import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js';
+import {
+  LitElement,
+  html,
+  css,
+} from "https://cdn.jsdelivr.net/gh/lit/dist@2/core/lit-core.min.js";
 
+import { TaskModel } from "../models.js";
 
 /**
- * WidgetBlock <widget-block header="Sample Widget">
- * Base example for a widget, used as a placeholder in design for unimplemented
- * widgets
+ shows the calendar for the current month and highlights the   current day and days where tasks are due.  Optionally, allows display of other months via interactive controls.
  */
 class CalenderWidget extends LitElement {
   static properties = {
@@ -14,11 +17,11 @@ class CalenderWidget extends LitElement {
 
   static styles = css`
     :host {
-        display: block;
-        width: 250px;
-        height: 250px;
-        background-color: azure;
-        border: 1px solid black;
+      display: block;
+      width: 250px;
+      height: 250px;
+      background-color: azure;
+      border: 1px solid black;
     }
   `;
 
@@ -29,11 +32,27 @@ class CalenderWidget extends LitElement {
     this.currentYear = today.getFullYear();
   }
 
+  DaysInMonth(month) {
+    //dayCount stores the amount of days in the called month
+    const dayCount = new Date(2024, month, 0).getDate();
+    //take the day count and make it an array, starting from 1 to dayCount
+    const days = [];
+    for (let i = 1; i <= dayCount; i++) {
+      days[i - 1] = i;
+    }
+    return days;
+  }
+
+  DueToday(day) {
+    //checking chosen day on current month
+    const date = new Date(this.currentYear, this.currentMonth, day);
+    //return true if task is due on specified day
+    return TaskModel.getTasksForDay(date).length > 0;
+  }
+
   render() {
-    return html`
-        <h3>${this.header}</h3>
-    `;
+    return html` <h3>${this.header}</h3> `;
   }
 }
 
-customElements.define('calender-widget', CalenderWidget);
+customElements.define("calender-widget", CalenderWidget);
